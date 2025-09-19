f89b58ecfc80124b7d1de19b2b0a63262ee689c0
stud5nt <serdaniil88@gmail.com>
2025-09-19 12:28:26 +0100

    Update main.py

--- my_package/main.py
+++ my_package/main.py
@@ -1,19 +1,20 @@
 """3. У файлі main.py імпортуй функції з модулів і продемонструй їх роботу, викликавши кожну з функцій."""
 
-
 from math_utils import factorial, ncd
 from string_utils import to_uppercase, trim_spaces
 
+
 def main():
-    """Use Math_utils"""
+    """math_utils"""
     print(f"Факторіал 4: {factorial(4)}")
     print(f"НСД чисел 40 та 10: {ncd(40, 10)}")
 
     print("------------------")
 
-    """Use String_utils"""
+    """string_utils"""
     print(f"Перетворення тексту в верхній регістр 'ithillel': {to_uppercase('ithillel')}")
     print(f"Видалення пробілів на початку та в кінці рядка '  hey  ': {trim_spaces('  hey  ')}")
+
 
 if __name__ == "__main__":
     main()
