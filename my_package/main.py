7f284055efd9e9901ba52dece1c8e0d0e478b13b
stud5nt <serdaniil88@gmail.com>
2025-09-19 12:02:22 +0100

    Update main.py

--- my_package/main.py
+++ my_package/main.py
@@ -5,13 +5,13 @@ from math_utils import factorial, ncd
 from string_utils import to_uppercase, trim_spaces
 
 def main():
-    """math_utils"""
-    print(f"Факторіал 5: {factorial(4)}")
-    print(f"НСД чисел 48 та 18: {ncd(40, 10)}")
+    """Use Math_utils"""
+    print(f"Факторіал 4: {factorial(4)}")
+    print(f"НСД чисел 40 та 10: {ncd(40, 10)}")
 
     print("------------------")
 
-    """string_utils"""
+    """Use String_utils"""
     print(f"Перетворення тексту в верхній регістр 'ithillel': {to_uppercase('ithillel')}")
     print(f"Видалення пробілів на початку та в кінці рядка '  hey  ': {trim_spaces('  hey  ')}")
 
