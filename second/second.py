5e5b0797b4ab46140d16b2395801c20f6fe2e08a
stud5nt <serdaniil88@gmail.com>
2025-09-19 12:27:35 +0100

    Update second.py

--- second/second.py
+++ second/second.py
@@ -3,8 +3,8 @@
 Напиши скрипт, який завантажує сторінку з вказаного URL та зберігає її вміст у текстовий файл.
 Додай обробку помилок на випадок, якщо сторінка недоступна."""
 
-
 import requests
+
 
 def download_page(url: str, output_file: str) -> None:
     """Download webpage and save to file"""
@@ -16,6 +16,7 @@ def download_page(url: str, output_file: str) -> None:
         print(f"Успішно збережено у файл з іменем -- {output_file}")
     except requests.exceptions.RequestException as e:
         print(f"Помилка при завантаженні файлу -- {e}")
+
 
 if __name__ == "__main__":
     download_page("https://google.com", "file.html")
