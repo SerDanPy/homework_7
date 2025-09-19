b62fa7c188840d0f9bea9a167b2f2e9e17d10cd8
stud5nt <serdaniil88@gmail.com>
2025-09-19 12:28:16 +0100

    Update math_utils.py

--- my_package/math_utils.py
+++ my_package/math_utils.py
@@ -1,9 +1,11 @@
 """Реалізуй у файлі math_utils.py функцію, яка обчислює факторіал числа, а також функцію для знаходження найбільшого спільного дільника двох чисел. """
 
+
 def factorial(n: int) -> int:
     if not isinstance(n, int) or n < 0:
         raise ValueError("Число повинно бути позитивним")
     return 1 if n == 0 else n * factorial(n - 1)
+
 
 def ncd(a: int, b: int) -> int:
     while b:
